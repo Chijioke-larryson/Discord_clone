8b6ce27c69db488a0f782303f8458ea18aa526b4
Larryson Chijioke <larrysongift@gmail.com>
2026-06-23 10:15:15 +0100

    Models added

--- base/admin.py
+++ base/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Room
+# from .models import Message, Topic
 
 admin.site.register(Room)
+# admin.site.register(Message)
+# admin.site.register(Topic)
--- base/views.py
+++ base/views.py
@@ -1,45 +1,19 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Room
 
-# # Create your views here.
-# def home(request):
-#     return render(request, 'home.html')
 
-# def room(request):
-#     return render(request, 'room.html')
-
-
-# rooms = [
-#     { "id" : 1 , "name": "Let us Learn Python"},
-#     { "id" : 2 , "name": "Let us Learn Javascript"},
-#     { "id" : 3 , "name": "Let us Learn Django"},
-#     { "id" : 4 , "name": "Code with me"},
-# ]
-
-# context = {'rooms': rooms}
-
-
-rooms = [
-    {"id" : 1 , "name": "Let us Learn Python"},
-     {"id" :2, "name": "Let us Learn Javascript"},
-    {"id" : 3 , "name": "Let us Learn Django"},
-    {"id" : 4 , "name": "Code with me"},
-    {"id" : 5 , "name": "Let us  answers questions"},
-]
-context = {'rooms': rooms}
 def home(request):
-    rooms = Room.objects.all()      
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
+
+
 def room(request, pk):
-    try:
-        room_id = int(pk)
-        room = next(i for i in rooms if i['id'] == room_id)
-    except (ValueError, StopIteration):
-        raise Http404('Room does not exist')
-
-    room = Room.objects.get(id=pk)
-    return render(request, 'base/room.html', {'room': room})
+    room = get_object_or_404(Room, id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
 
-    
+# def createRoom(request):
+#     context = {}
+#     return render(request, 'base/room_form.html', context)
